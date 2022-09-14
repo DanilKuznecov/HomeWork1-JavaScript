ffbce649f5a6639cf8f328d60b367829257aff0c
Danil <danilkuznecov15@gmail.com>
2022-09-14 23:50:36 +0500

    JavaScript-1

---
+++ script.js
@@ -0,0 +1,84 @@
+var name = prompt('Введите своё имя.')
+
+console.log(name);
+
+
+
+var age = +prompt('Введите Ваш возраст')
+
+console.log(age);
+
+
+
+
+var pl = +prompt('4 + 6 = ?')
+console.log('4 + 6 = ?');
+console.log(pl);
+alert('Ваш ответ: ' + pl)
+console.log('4 + 6 = '  + (4 + 6) + '    Ваш ответ: ' + pl);
+alert('Правильный ответ: 10')
+
+
+
+
+
+var ms = +prompt('10 - 6 = ?')
+console.log('10 - 6 = ?');
+console.log(ms);
+alert('Ваш ответ: ' + ms)
+console.log('10 - 6 = '  + (10 - 6) + '    Ваш ответ: ' + ms);
+alert('Правильный ответ: 4')
+
+
+
+
+
+var um = +prompt('4 * 6 = ?')
+console.log('4 * 6 = ?');
+console.log(um);
+alert('Ваш ответ: ' + um)
+console.log('4 * 6 = '  + 4 * 6 + '    Ваш ответ: ' + um);
+alert('Правильный ответ: 24')
+
+
+
+
+
+var dl = +prompt('24 / 6 = ?')
+console.log('24 / 6 = ?');
+console.log(dl);
+alert('Ваш ответ: ' + dl)
+console.log('24 / 6 = '  + 24 / 6 + '    Ваш ответ: ' + dl);
+alert('Правильный ответ: 4')
+
+
+
+
+
+var os = +prompt('27 % 6 = ?')
+console.log('27 % 6 = ?');
+console.log(os);
+alert('Ваш ответ: ' + os)
+console.log('27 % 6 = '  + 27 % 6 + '    Ваш ответ: ' + os);
+alert('Правильный ответ: 3')
+
+
+
+
+console.log('ЗАДАНИЕ №2');
+
+
+
+var X = +prompt('Введите первое число')
+console.log(X);
+var Y = +prompt('Введите второе число')
+console.log(Y);
+var Z = +prompt('Введите третье число')
+console.log(Z);
+
+var res = ((X + Y + Z) / 3)
+alert(res)
+
+
+
+
